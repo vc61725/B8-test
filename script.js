775b782c874234cb6663e8de2b90bcd0ccfd44c5
vc61725 <a0970224777@gmail.com>
2026-06-08 16:33:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
 function convertCSV() {
-    alert("BIG5測試版");
+
+
+const file =
+    document.getElementById("csvFile").files[0];
+
+if (!file) {
+    alert("請先選擇CSV");
+    return;
+}
+
+const reader = new FileReader();
+
+reader.onload = function(e) {
+
+    const decoder =
+        new TextDecoder("big5");
+
+    const text =
+        decoder.decode(
+            e.target.result
+        );
+
+    console.clear();
+
+    console.log(
+        text.substring(0, 3000)
+    );
+
+    alert(
+        "已輸出前3000字到Console"
+    );
+
+};
+
+reader.readAsArrayBuffer(file);
+
+
 }
