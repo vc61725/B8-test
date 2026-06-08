31fefc88fc5002993c51148fab37eb2d4fe00c90
vc61725 <a0970224777@gmail.com>
2026-06-08 16:44:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,37 @@ reader.onload = function(e) {
     const lines =
         text.split(/\r?\n/);
 
+    let warehouse = "";
+    let noticeNo = "";
+
+    lines.forEach(line => {
+
+        if (
+            line.includes("2944-QF")
+        ) {
+
+            const cols =
+                line.split(",");
+
+            noticeNo =
+                cols[2] || "";
+        }
+
+        if (
+            line.includes("物流中心")
+            &&
+            line.includes("大肚")
+        ) {
+
+            const cols =
+                line.split(",");
+
+            warehouse =
+                cols[1] || "";
+        }
+
+    });
+
     let startIndex = -1;
 
     lines.forEach(
@@ -44,32 +75,85 @@ reader.onload = function(e) {
 
     if(startIndex < 0){
 
-        alert("找不到表頭");
+        alert("找不到商品資料");
         return;
 
     }
 
-    const firstRow =
-        lines[startIndex + 1];
+    const result = [];
 
-    const cols =
-        firstRow.split(",");
+    for(
+        let i=startIndex+1;
+        i<lines.length;
+        i++
+    ){
 
-    console.clear();
+        const cols =
+            lines[i].split(",");
 
-    cols.forEach(
-        (value,index)=>{
+        if(cols.length < 18)
+            continue;
 
-            console.log(
-                index,
-                value
-            );
+        result.push({
 
-        }
+            "採購日期":
+                cols[17],
+
+            "進貨指定日":
+                cols[17],
+
+            "採購通知單代號":
+                noticeNo,
+
+            "進貨倉庫":
+                warehouse,
+
+            "序號":
+                i - startIndex,
+
+            "訂貨驗收單":
+                cols[0],
+
+            "品名":
+                cols[2],
+
+            "品代":
+                cols[3],
+
+            "訂購總數":
+                cols[7],
+
+            "條碼":
+                cols[15],
+
+            "物流中心":
+                cols[16]
+
+        });
+
+    }
+
+    const wb =
+        XLSX.utils.book_new();
+
+    const ws =
+        XLSX.utils.json_to_sheet(
+            result
+        );
+
+    XLSX.utils.book_append_sheet(
+        wb,
+        ws,
+        "進貨資料"
+    );
+
+    XLSX.writeFile(
+        wb,
+        "轉換完成.xlsx"
     );
 
     alert(
-        "請到F12 Console複製內容給ChatGPT"
+        "轉換完成"
     );
 
 };
@@ -78,3 +162,4 @@ reader.readAsArrayBuffer(file);
 
 
 }
+
