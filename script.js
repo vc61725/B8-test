9aed6263dbdb5fda9084c5c96cd136543b3c7067
vc61725 <a0970224777@gmail.com>
2026-06-08 16:25:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,68 @@
 function convertCSV() {
-alert("測試成功");
+
+alert("開始讀取檔案");
+
+const file =
+    document.getElementById("csvFile").files[0];
+
+if (!file) {
+    alert("請先選擇CSV");
+    return;
 }
+
+const reader = new FileReader();
+
+reader.onload = function(e) {
+
+    alert("檔案已讀取");
+
+    try {
+
+        const workbook = XLSX.read(
+            e.target.result,
+            {
+                type: "binary"
+            }
+        );
+
+        alert("XLSX解析成功");
+
+        const sheet =
+            workbook.Sheets[
+                workbook.SheetNames[0]
+            ];
+
+        const rows =
+            XLSX.utils.sheet_to_json(
+                sheet,
+                {
+                    header: 1,
+                    defval: ""
+                }
+            );
+
+        console.log(rows);
+
+        alert(
+            "資料列數：" +
+            rows.length
+        );
+
+    }
+    catch(err) {
+
+        console.error(err);
+
+        alert(
+            "錯誤：" +
+            err.message
+        );
+
+    }
+
+};
+
+reader.readAsBinaryString(file);
+
+}
+
