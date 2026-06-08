4769640e38a903a8a8924048918a113115d958f4
vc61725 <a0970224777@gmail.com>
2026-06-08 16:32:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
 function convertCSV() {
-
-```
-const file =
-    document.getElementById("csvFile").files[0];
-
-if (!file) {
-    alert("請先選擇CSV");
-    return;
-}
-
-const reader = new FileReader();
-
-reader.onload = function(e) {
-
-    const decoder =
-        new TextDecoder("big5");
-
-    const text =
-        decoder.decode(
-            e.target.result
-        );
-
-    console.log(text);
-
-    alert(
-        "已用Big5讀取，請查看Console"
-    );
-
-};
-
-reader.readAsArrayBuffer(file);
-```
-
+    alert("BIG5測試版");
 }
