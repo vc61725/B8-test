6e2ef9b6e6d9e4ba5f1950dc2fd559d7c3a9dce9
vc61725 <a0970224777@gmail.com>
2026-06-08 16:39:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,46 +24,6 @@ reader.onload = function(e) {
     const lines =
         text.split(/\r?\n/);
 
-    const result = [];
-
-    let purchaseDate = "";
-    let warehouse = "";
-    let noticeNo = "";
-
-    for (const line of lines) {
-
-        if (
-            line.includes("採購日期")
-        ) {
-
-            const match =
-                line.match(
-                    /\d{4}\/\d{1,2}\/\d{1,2}/
-                );
-
-            if (match)
-                purchaseDate =
-                match[0];
-        }
-
-        if (
-            line.includes("物流中心")
-        ) {
-
-            warehouse =
-                line.split(",")[1] || "";
-        }
-
-        if (
-            line.includes("2944-QF")
-        ) {
-
-            noticeNo =
-                line.split(",")[2] || "";
-        }
-
-    }
-
     let startIndex = -1;
 
     lines.forEach(
@@ -84,81 +44,32 @@ reader.onload = function(e) {
 
     if(startIndex < 0){
 
-        alert("找不到商品資料表頭");
+        alert("找不到表頭");
         return;
 
     }
 
-    for(
-        let i=startIndex+1;
-        i<lines.length;
-        i++
-    ){
+    const firstRow =
+        lines[startIndex + 1];
 
-        const cols =
-            lines[i].split(",");
+    const cols =
+        firstRow.split(",");
 
-        if(
-            cols.length < 10
-        )
-            continue;
+    console.clear();
 
-        result.push({
+    cols.forEach(
+        (value,index)=>{
 
-            採購日期:
-                purchaseDate,
+            console.log(
+                index,
+                value
+            );
 
-            進貨指定日:
-                purchaseDate,
-
-            採購通知單代號:
-                noticeNo,
-
-            進貨倉庫:
-                warehouse,
-
-            序號:
-                cols[0],
-
-            訂貨驗收單:
-                cols[1],
-
-            品名:
-                cols[2],
-
-            品代:
-                cols[3],
-
-            訂購總數:
-                cols[7],
-
-            條碼:
-                cols[16],
-
-            物流中心:
-                cols[17]
-
-        });
-
-    }
-
-    const wb =
-        XLSX.utils.book_new();
-
-    const ws =
-        XLSX.utils.json_to_sheet(
-            result
-        );
-
-    XLSX.utils.book_append_sheet(
-        wb,
-        ws,
-        "進貨資料"
+        }
     );
 
-    XLSX.writeFile(
-        wb,
-        "轉換完成.xlsx"
+    alert(
+        "請到F12 Console複製內容給ChatGPT"
     );
 
 };
