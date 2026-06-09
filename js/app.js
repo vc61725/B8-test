03b3316fce48604875458cc9d38d5b4b870016c6
vc61725 <a0970224777@gmail.com>
2026-06-09 11:59:28 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("B8 System");
+function convertCSV() {
+
+    const file =
+        document.getElementById("csvFile").files[0];
+
+    if (!file) {
+
+        alert("請先選擇 CSV 檔案");
+
+        return;
+    }
+
+    document.getElementById("result").innerHTML =
+        "CSV 已成功選取：<br><br>" + file.name;
+}
