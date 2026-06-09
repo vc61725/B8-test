2c1ac607ea9a9802e49ec085cfebf0f67dabc130
vc61725 <a0970224777@gmail.com>
2026-06-09 13:17:06 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
+let b8Data = [];
+
+window.onload = async function () {
+
+    try {
+
+        const response =
+            await fetch("./data/B8.json");
+
+        b8Data = await response.json();
+
+        document.getElementById("result").innerHTML =
+            `成功載入 ${b8Data.length} 筆商品資料`;
+
+        console.log(b8Data);
+
+    } catch (error) {
+
+        console.error(error);
+
+        document.getElementById("result").innerHTML =
+            "B8資料載入失敗";
+    }
+};
+
 function convertCSV() {
 
     const file =
@@ -5,11 +30,12 @@ function convertCSV() {
 
     if (!file) {
 
-        alert("請先選擇 CSV 檔案");
+        alert("請先選擇CSV檔");
 
         return;
     }
 
     document.getElementById("result").innerHTML =
-        "CSV 已成功選取：<br><br>" + file.name;
+        `B8資料筆數：${b8Data.length}<br><br>` +
+        `CSV檔案：${file.name}`;
 }
