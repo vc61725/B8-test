1a9596bb4dbc1fba700ca2b030847112ed63323b
vc61725 <a0970224777@gmail.com>
2026-06-10 13:34:58 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -188,27 +188,74 @@ csvData.forEach(item => {
 
 });
 
-console.log(resultData);
+let html = `
 
-console.log(notFoundList);
+<h3>入倉通知單</h3>
 
-        document.getElementById("result").innerHTML =
-`
-成功 Mapping：
+<table>
 
-${resultData.length} 筆
-
-找不到：
-
-${notFoundList.length} 筆
+<tr>
+<th>配送中心</th>
+<th>配送日期</th>
+<th>廠商</th>
+<th>TDC</th>
+<th>品名</th>
+<th>箱數</th>
+<th>日翊代號</th>
+<th>國際條碼</th>
+</tr>
 `;
-        
-        document.getElementById("result").innerHTML = `
-成功讀取 ${csvData.length} 筆 CSV 資料
-<br>
-TDC：
-${csvData[0]?.tdc}
+
+resultData.forEach(item => {
+
+```
+html += `
+<tr>
+
+    <td>${item.center}</td>
+
+    <td>${item.deliveryDate}</td>
+
+    <td>${item.vendorName}</td>
+
+    <td>${item.tdc}</td>
+
+    <td>${item.productName}</td>
+
+    <td>${item.cartonQty}</td>
+
+    <td>${item.riyiCode}</td>
+
+    <td>${item.barcode}</td>
+
+</tr>
 `;
+```
+
+});
+
+html += `
+
+</table>
+`;
+
+if (notFoundList.length > 0) {
+
+```
+html += `
+<hr>
+
+<h3 style="color:red">
+⚠ 找不到 Mapping 商品
+</h3>
+`;
+```
+
+}
+
+document.getElementById("result").innerHTML =
+html;
+
 
     };
 
